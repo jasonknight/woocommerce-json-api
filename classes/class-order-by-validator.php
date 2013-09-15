1f43840b935269bd355de323825344b85933e320
Jason Knight <jason@jason-knight-martin.com>
2013-09-15 13:47:11 -0400

    Tweaking validators

--- classes/class-order-by-validator.php
+++
@@ -1,7 +0,0 @@
-<?php
-class OrderByValidator {
-  public function validate( $source, &$value, &$target ) {
-    JSONAPIHelpers::debug("Beggining validation of OrderBy");
-    $target->addWarning("Looks like I can edit things",-1);
-  }
-}
