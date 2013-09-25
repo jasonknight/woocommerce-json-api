8dfc4ba02df852d9ccffdd6246e35b564dada49c
Jason Knight <jason@jason-knight-martin.com>
2013-09-25 05:01:28 -0400

    Added color to the debug output...just for kicks

--- classes/WCAPI/BaseHelpers.php
+++ classes/WCAPI/BaseHelpers.php
@@ -66,6 +66,34 @@ class Helpers {
     }
     $fp = @fopen(REDE_PLUGIN_BASE_PATH . "debug.log",'a');
     if ($fp) {
+      $cmd = new CmdColors();
+      $keywords = array('SELECT','FROM','WHERE','AND','NOT','IN','UPDATE','SET','INSERT','VALUES','=>','AS','=','FALSE','false','TRUE','true');
+      foreach ($keywords as $kw ) {
+        $nkw = $cmd->getColoredString($kw,'red');
+        $text = str_replace($kw,$nkw,$text);
+      }
+      $text = preg_replace_callback("/[A-Z]{1}[\w_]+::[a-z]{1}[\w]+/", function($matches) {
+        $colors = new CmdColors();
+        $parts = explode("::",$matches[0]);
+        return $colors->getColoredString($parts[0],'blue') . "::" . $colors->getColoredString($parts[1],'cyan');
+      }, $text);
+      
+      $text = preg_replace_callback("/'[\w\-\d\/]+'/", function($matches) {
+        $colors = new CmdColors();
+        return $colors->getColoredString($matches[0],'yellow');
+      }, $text);
+      $text = preg_replace_callback("/\"[\w\-\d\/]+\"/", function($matches) {
+        $colors = new CmdColors();
+        return $colors->getColoredString($matches[0],'yellow');
+      }, $text);
+      $text = preg_replace_callback("/wp_[\w]+|post_[\w]+|term_[\w]+|comment_[\w]+|[a-z]+\./", function($matches) {
+        $colors = new CmdColors();
+        return $colors->getColoredString($matches[0],'light_purple');
+      }, $text);
+      $text = preg_replace_callback("/Closure/", function($matches) {
+        $colors = new CmdColors();
+        return $colors->getColoredString($matches[0],'green');
+      }, $text);
       fwrite($fp,$text . "\n");
       fclose($fp);
     }
@@ -391,4 +419,83 @@ class Helpers {
  }
   
 }
-?>
+if (! class_exists('CmdColors') ) {
+  class CmdColors {
+    private $foreground_colors = array();
+    private $background_colors = array();
+    public function __construct() {
+      // Set up shell colors
+      $this->foreground_colors['black'] = '0;30';
+      $this->foreground_colors['dark_gray'] = '1;30';
+      $this->foreground_colors['blue'] = '0;34';
+      $this->foreground_colors['light_blue'] = '1;34';
+      $this->foreground_colors['green'] = '0;32';
+      $this->foreground_colors['light_green'] = '1;32';
+      $this->foreground_colors['cyan'] = '0;36';
+      $this->foreground_colors['light_cyan'] = '1;36';
+      $this->foreground_colors['red'] = '0;31';
+      $this->foreground_colors['light_red'] = '1;31';
+      $this->foreground_colors['purple'] = '0;35';
+      $this->foreground_colors['light_purple'] = '1;35';
+      $this->foreground_colors['brown'] = '0;33';
+      $this->foreground_colors['yellow'] = '1;33';
+      $this->foreground_colors['light_gray'] = '0;37';
+      $this->foreground_colors['white'] = '1;37';
+
+      $this->background_colors['black'] = '40';
+      $this->background_colors['red'] = '41';
+      $this->background_colors['green'] = '42';
+      $this->background_colors['yellow'] = '43';
+      $this->background_colors['blue'] = '44';
+      $this->background_colors['magenta'] = '45';
+      $this->background_colors['cyan'] = '46';
+      $this->background_colors['light_gray'] = '47';
+    }
+
+    // Returns colored string
+    public function getColoredString($string, $foreground_color = null, $background_color = null) {
+      $colored_string = "";
+
+      // Check if given foreground color found
+      if (isset($this->foreground_colors[$foreground_color])) {
+        $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+      }
+      // Check if given background color found
+      if (isset($this->background_colors[$background_color])) {
+        $colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+      }
+
+      // Add string and end coloring
+      $colored_string .=  $string . "\033[0m";
+
+      return $colored_string;
+    }
+
+    // Returns all foreground color names
+    public function getForegroundColors() {
+      return array_keys($this->foreground_colors);
+    }
+
+    // Returns all background color names
+    public function getBackgroundColors() {
+      return array_keys($this->background_colors);
+    }
+
+    public function _( $str, $color, $bg = null) {
+      static $last = 'black';
+      if ( $bg ) {
+        echo($this->getColoredString($str, $color, $bg));
+        return;
+      } else {
+        echo($this->getColoredString($str, $color, $last));
+      }
+      
+      if ( $last == 'none' && ($str == "FAILED" || $str == "PASSED") ) {
+        $last = 'black';
+      } else if ($last == 'black' && ($str == "FAILED" || $str == "PASSED") ) {
+        $last = 'none';
+      }
+
+    }
+  }
+}
