572e416d5241f66764ed90fdd7124922913341e1
Jason Knight <jason@jason-knight-martin.com>
2013-12-01 09:22:49 -0500

    various debuggin

--- classes/WCAPI/Order.php
+++ classes/WCAPI/Order.php
@@ -263,6 +263,9 @@ class Order extends Base {
     // if ( isset($this->_meta_attributes['status']) && !empty($this->_meta_attributes['status']) ) {
     //   return $this->_meta_attributes['status'];
     // }
+    Helpers::debug("Using WooCom to get status");
+    $order = new WC_Order($this->_actual_model_id);
+    return $order->status;
     $sql = "
       SELECT
         t.slug
